6a65679ed27f2bcf9a843616538cf245feb9c313
Adedayo Ezekiel Adejumo <167407110+Easysoul1@users.noreply.github.com>
2025-01-30 11:08:02 +0100

    Remove .git from the github_url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
        res.json({
               "email": 'adejumoadedayo350@gmail.com',
               "current_datetime": new Date().toISOString(),
-              "github_url": 'https://github.com/Easysoul1/HNG-INTERNSHIP.git'
+              "github_url": 'https://github.com/Easysoul1/HNG-INTERNSHIP'
        });
 });
 
